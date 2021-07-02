0589b292613d2811e7304d32e862a106b4a34ed3
DanielleLiu <DanielleLiu@users.noreply.github.com>
2021-07-02 10:18:40 -0400

    initial commit

---
+++ ExperimentalGUI/profiles/Intervention/CreateProfiles.m
@@ -0,0 +1,128 @@
+%% Intervention Profile
+velL = velL';
+velR = velR';
+save('TMBaselineSlow.mat','velL','velR')
+
+%% adaptation 1 - baseline adaptation
+velL = [0.75*ones(1,150),0.5*ones(1,150),0.75*ones(1,50)];
+velR = [0.75*ones(1,150),1*ones(1,150),0.75*ones(1,50)];
+save('Adaptation1_RightDominant.mat','velL','velR')
+
+velR = [0.75*ones(1,150),0.5*ones(1,150),0.75*ones(1,50)];
+velL = [0.75*ones(1,150),1*ones(1,150),0.75*ones(1,50)];
+save('Adaptation1_LeftDominant.mat','velL','velR')
+
+%% switching/adaptation 2 and 3
+velL = [0.5*ones(1,150),0.75*ones(1,50),0.5*ones(1,150),0.75*ones(1,50)];
+velR = [1*ones(1,150),0.75*ones(1,50),1*ones(1,150),0.75*ones(1,50)];
+save('Adaptation23_RightDominant.mat','velL','velR')
+
+vel=velR;
+velR = velL;
+velL = vel;
+save('Adaptation23_LeftDominant.mat','velL','velR')
+
+%% 
+velL = [0.5*ones(1,150)];
+velR = [1*ones(1,150)];
+save('Adaptation4_RightDominant.mat','velL','velR')
+
+vel=velR;
+velR = velL;
+velL = vel;
+save('Adaptation4_LeftDominant.mat','velL','velR')
+
+%% post-adapataion
+velL = [0.75*ones(1,315)];
+velR = [0.75*ones(1,315)];
+save('OGPost.mat','velL','velR')
+
+velL = [0.75*ones(1,150)];
+velR = [0.75*ones(1,150)];
+save('TMPost.mat','velL','velR')
+
+%% plot full protocol - intervention
+velDom = [0.75*ones(1,150),0.5*ones(1,150),1*ones(1,150),0.75*ones(1,150)];
+% line([0,150],[0.75,0.75],'LindWidth',')
+velNonDom = velDom;
+for i = 1:5
+    velDom = [velDom,1*ones(1,150),0.75*ones(1,50)];
+    velNonDom = [velNonDom, 0.5*ones(1,150),0.75*ones(1,50)];
+end
+velDom = [velDom, 1*ones(1,150), 0.75*ones(1,600)];
+velNonDom = [velNonDom, 1*ones(1,150), 0.75*ones(1,600)];
+
+close all
+figure;
+area([600,1750],[1,1],0.5,'FaceColor','#edebe4','EdgeColor','#edebe4');
+
+hold on;
+area([0,150],[1,1],0.5,'FaceColor','#e4f7f6','EdgeColor','#e4f7f6');
+area([1750,2200],[1,1],0.5,'FaceColor','#e4f7f6','EdgeColor','#e4f7f6');
+h(1) = plot(velDom, 'LineWidth',4,'Color','#0072BD');
+h(2) = plot(velNonDom, 'LineWidth',3,'Color','#D95319');
+ylabel('Speed (m/s)')
+xlabel('Stride')
+
+xticks([150,300,450,600,750,800,950,1000,1150,1200,1350,1400,1550,1600,1750,2200,2350])
+xlim([0,2350])
+yticks([0.5,0.75,1])
+a = get(gca,'XTickLabel');  
+set(gca,'XTickLabel',a,'fontsize',15)
+
+breakloc = [300,450,800,1200,1600,2050];
+for i = breakloc
+    xline(i,'k--','LineWidth',2);
+end
+f= get(gca,'Children');
+legend(f([1, 7, 8,9,end]),'Break (5mins)','Non-Dominant Leg','Dominant Leg','Overground','Switching');
+
+delete(findall(gcf,'type','annotation'));
+annotation('textbox', [.13,.001,.05,.05],'String','Overground','EdgeColor', 'none' ,'FontSize', 16,'Color','k','fontweight', 'bold');
+annotation('textbox', [.22,.001,.07,.05],'String','TM Baseline','EdgeColor', 'none' ,'FontSize', 16,'Color','k','fontweight', 'bold');
+annotation('textbox', [.45,.001,.09,.05],'String','Adaptation/Switching','EdgeColor', 'none' ,'FontSize', 16,'Color','k','fontweight', 'bold');
+annotation('textbox', [.78,.001,.05,.05],'String','Overground','EdgeColor', 'none' ,'FontSize', 16,'Color','k','fontweight', 'bold');
+annotation('textbox', [.85,.001,.07,.05],'String','TM Washout','EdgeColor', 'none' ,'FontSize', 16,'Color','k','fontweight', 'bold');
+
+%% plot full protocol - pre and post intervention
+velDom = [0.75*ones(1,150),0.5*ones(1,150),1*ones(1,150),0.75*ones(1,150)];
+% line([0,150],[0.75,0.75],'LindWidth',')
+velNonDom = velDom;
+velDom = [velDom,1*ones(1,900),0.75*ones(1,600)];
+velNonDom = [velNonDom,0.5*ones(1,900),0.75*ones(1,600)];
+
+close all
+figure;
+% adaptation area
+area([600,1500],[1,1],0.5,'FaceColor','#edebe4','EdgeColor','#edebe4');
+
+hold on;
+%OG area
+area([0,150],[1,1],0.5,'FaceColor','#e4f7f6','EdgeColor','#e4f7f6');
+area([1500,1950],[1,1],0.5,'FaceColor','#e4f7f6','EdgeColor','#e4f7f6');
+plot(velDom, 'LineWidth',4,'Color','#0072BD');
+plot(velNonDom, 'LineWidth',3,'Color','#D95319');
+
+xticks([150,300,450,600,1500,1950,2100])
+xlim([0,2100])
+yticks([0.5,0.75,1])
+ylabel('Speed (m/s)')
+% xlabel('Stride')
+a = get(gca,'XTickLabel');  
+set(gca,'XTickLabel',a,'fontsize',15)
+
+breakloc = [300,750,1050,1350,1800];
+for i = breakloc
+    xline(i,'k--','LineWidth',2);
+end
+f= get(gca,'Children');
+legend(f([1, 6, 7,8,end]),'Break (5mins)','Non-Dominant Leg','Dominant Leg','Overground','Adaptation');
+
+delete(findall(gcf,'type','annotation'));
+annotation('textbox', [.13,.001,.05,.05],'String','Overground','EdgeColor', 'none' ,'FontSize', 16,'Color','k','fontweight', 'bold');
+annotation('textbox', [.24,.001,.09,.05],'String','TM Baseline','EdgeColor', 'none' ,'FontSize', 16,'Color','k','fontweight', 'bold');
+annotation('textbox', [.5,.001,.07,.05],'String','Adaptation','EdgeColor', 'none' ,'FontSize', 16,'Color','k','fontweight', 'bold');
+annotation('textbox', [.73,.001,.07,.05],'String','Overground','EdgeColor', 'none' ,'FontSize', 16,'Color','k','fontweight', 'bold');
+annotation('textbox', [.85,.001,.09,.05],'String','TM Washout','EdgeColor', 'none' ,'FontSize', 16,'Color','k','fontweight', 'bold');
+
+
---
+++ ProcessingData.m
@@ -0,0 +1,215 @@
+%% Load data - new
+raw = load('/Users/mac/Desktop/Lab/SMLLab/Code/NIRS_Code/data/S06V01/S06V01M0102.mat');
+%only the 2nd one is valid
+raw = raw.raw(2,1);
+%% plot the raw data
+for i=1:length(raw)
+    figure();
+    raw(i).draw
+end
+%% now processing
+
+j=nirs.modules.RemoveStimless;
+j=nirs.modules.OpticalDensity(j);
+j=nirs.modules.Resample(j);
+j=nirs.modules.BeerLambertLaw(j);
+Hb=j.run(raw);
+
+for i=1:length(Hb)
+    figure();
+    Hb(i).draw
+end
+
+%% Rename stimulus conditions to avoid space
+idx = [7,8,9,11];
+keys = stimulus.keys;
+keys
+for id = idx
+    key = keys{id};
+    splitkeys = split(key);
+    combinedkey = [splitkeys{1}];
+    for wordidx = 2:length(splitkeys)
+        combinedkey = [combinedkey,'_',splitkeys{wordidx}]
+    end
+    keys{id} = combinedkey;
+end
+
+keys
+stimulus.keys = keys;
+
+%% rename stimulus values to avoid space
+
+%% GLM
+j=nirs.modules.GLM;
+j.goforit=true;
+SubjStats=j.run(Hb);
+% active vs rest comparison 
+SubjStats=SubjStats.ttest({'Even-Rest_Even'
+                           'Even_ABC-Rest_Even_ABC'
+                           'Uneven-Rest_Uneven'
+                           'Uneven_ABC-Rest_Uneven_ABC'
+                           'Standing_ABC-Rest_Standing_ABC'},[],...
+                           {'Even','Even_ABC','Uneven','Uneven_ABC','Standing_ABC'});
+% among active/walking conditions comparison                        
+SubjStats2=SubjStats.ttest({'Uneven-Even'
+                            'Even_ABC-Even'
+                            'Uneven_ABC-Even'}, [], ...
+                            {'Uneven-Even', 'Even_ABC-Even', 'Uneven_ABC-Even'});
+ 
+%% exclude flagging data
+%022 (1) T1 even_ABC
+height(SubjStats(81).variables)
+for i=1:height(SubjStats(81).variables);
+  if isequal(SubjStats(81).variables.cond(i),{'Even_ABC'});
+  SubjStats(81).variables{i,4}={''}; 
+  %SubjStats(81).tstat(i)=missing;
+  %SubjStats(81).p(i)=missing;
+  %SubjStats(81).q(i)=missing;
+  %disp(['SubjStats' num2str(i) 'is flagged'])
+  end;
+end;
+%  (2) T1 even_ABC-even
+for i=1:height(SubjStats2(81).variables);
+  if isequal(SubjStats2(81).variables.cond(i), {'Even_ABC-Even'});
+      SubjStats2(81).variables{i, 4}={''};
+  end;
+end;                  
+%024 T1 standing_ABC2 ?
+for i=1:height(SubjStats(89).variables);
+  if isequal(SubjStats(89).variables.cond(i),{'Standing_ABC'});
+  SubjStats(89).variables.cond(i)={''};  
+  end; 
+end;
+%027 T1 standing_ABC 1,2
+for i=1:height(SubjStats(97).variables);
+    if isequal(SubjStats(97).variables.cond(i),{'Standing_ABC'});
+    SubjStats(97).variables.cond(i)={''};    
+    end;
+end;
+%028 T1 standing_ABC2 ?
+for i=1:height(SubjStats(101).variables);
+    if isequal(SubjStats(101).variables.cond(i),{'Standing_ABC'});
+    SubjStats(101).variables.cond(i)={''};    
+    end;
+end;
+%022 T2 standing_ABC2
+for i=1:height(SubjStats(82).variables);
+    if isequal(SubjStats(82).variables.cond(i),{'Standing_ABC'});
+    SubjStats(82).variables.cond(i)={''};    
+    end;
+end;
+%027 (1) T4 even_ABC
+for i=1:height(SubjStats(100).variables);
+    if isequal(SubjStats(100).variables.cond(i),{'Even_ABC'});
+    SubjStats(100).variables.cond(i)={''};    
+    end;
+end;
+     %(2) even_ABC-even
+for i=1:height(SubjStats2(100).variables);
+    if isequal(SubjStats2(100).variables.cond(i),{'Even_ABC-Even'});
+    SubjStats2(100).variables.cond(i)={''};
+    end;
+end;
+%% Output channel-wise SubjStats table
+j=nirs.modules.SubjLevelStats;
+SubjStats=j.run(SubjStats);
+SubjStats2=j.run(SubjStats2);
+%for i=1:length(SubjStats);
+%    SubjStats(i).draw
+%    figure()
+%end;
+%SubjStatsDiff=SubjStats.ttest({     'Uneven-Even' 
+%                                    'Even_ABC-Even'
+%                                    'Even_ABC-Standing_ABC' 
+%                                    'Uneven_ABC-Even'
+%                                    'Uneven_ABC-Standing_ABC'},[],...
+%                                    {'Ctrast1','Ctrast2','Ctrast3','Ctrast4','Ctrast5'});
+
+SubjStatstable=[];
+for i=1:length(SubjStats);
+  SubjStatstable=vertcat(SubjStatstable, SubjStats(i).table);
+end;
+SubjStatstable=rmmissing(SubjStatstable, 'DataVariables', {'cond'});
+nirs.createDemographicsTable(SubjStats).subject;
+id=[1:29]';
+Subjid=array2table(repelem(id,80),'VariableNames',{'Subjid'});
+SubjStatstable=[Subjid SubjStatstable];
+writetable(SubjStatstable,'SubjStats_channelw.xls','sheet',1);
+
+SubjStatstable2=[];
+for i=1:length(SubjStats2);
+    SubjStatstable2=vertcat(SubjStatstable2, SubjStats2(i).table);
+end;
+SubjStatstable2=rmmissing(SubjStatstable2, 'DataVariables', {'cond'});
+id=nirs.createDemographicsTable(SubjStats2).subject;
+Subjid=array2table(repelem(id, 48), 'VariableNames', {'Subjid'});
+SubjStatstable2=[Subjid SubjStatstable2];
+writetable(SubjStatstable2, 'SubjStatsdiff_channelw.csv');
+                                
+%% Group based model-- without adjustment
+j=nirs.modules.MixedEffects;
+j.formula='beta ~ -1 + cond + (1|subject)';
+GroupStats= j.run(SubjStats);
+
+GroupStats.draw('tstat',[],'q<0.05')
+
+
+%% ROI wise
+% define some ROIs
+j=nirs.modules.SubjLevelStats;
+SubjStats=j.run(SubjStats);
+ROI{1}=table(1,NaN,'VariableNames',{'detector','source'});
+ROI{2}=table(2,NaN,'VariableNames',{'detector','source'});
+%nirs.util.roiAverage(GroupStats,ROI,{'Det1','Det2'})
+table1=nirs.util.roiAverage(SubjStats,ROI,{'Det1','Det2'});
+
+table1=rmmissing(table1, 'DataVariables', {'Contrast'});
+writetable(table1,'SubjStats_ROIw.xlsx','sheet',1);
+
+table_diff=nirs.util.roiAverage(SubjStatsDiff,ROI,{'Det1','Det2'});
+writetable(table_diff,'SubjStatsDiff.xls','sheet',1);
+
+%combined
+ROIc=table(NaN,NaN,'VariableNames',{'detector','source'});
+table2=nirs.util.roiAverage(SubjStats,ROIc,{'PFC'});
+writetable(table2,'SubjStatsc.xls','sheet',1);      
+
+%performance variables
+speed=getspeed(xlfile);
+writetable(speed,'speed.xls','sheet',1);
+
+%% ROI wise 2
+% define some ROIs
+j=nirs.modules.SubjLevelStats;
+SubjStats=j.run(SubjStats);
+ROI{1}=table(1,NaN,'VariableNames',{'detector','source'});
+ROI{2}=table(2,NaN,'VariableNames',{'detector','source'});
+%nirs.util.roiAverage(GroupStats,ROI,{'Det1','Det2'})
+table1=nirs.util.roiAverage(SubjStats,ROI,{'Det1','Det2'});
+
+table1=rmmissing(table1, 'DataVariables', {'Contrast'});
+writetable(table1,'SubjStats_ROIw.xlsx','sheet',1);
+
+%% add demographic data
+nirs.createDemographicsTable(raw)
+
+demo = readtable('NMCM demo.xlsx');
+
+
+job = nirs.modules.AddDemographics;
+job.demoTable=demo;
+SubjStats = job.run(SubjStats);
+
+%% Group based models--adjusting for demographics
+job = nirs.modules.SubjLevelStats;
+SubjStats=job.run(SubjStats);
+
+job = nirs.modules.MixedEffects;
+job.formula='beta ~ -1 + cond + cond:Age + Sex +(1|subject)';
+G=job.run(SubjStats);
+
+G.draw('tstat',[],'q<0.05')
+
+
+%% Load GUI
+nirs.viz.nirsviewer;
