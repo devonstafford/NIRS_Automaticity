20fe2a3d581b22829c99cfc2ebd5a0b0abb7b9e8
DanielleLiu <DanielleLiu@users.noreply.github.com>
2021-08-17 14:10:35 -0400

    Update AddingConditionsNirs.m
    Support adding trials into existing conditions, however, the data seems to be modified after saving.

--- AddingConditionsNirs.m
+++ AddingConditionsNirs.m
@@ -1,44 +1,80 @@
 %Chagingparams, create new conditions in 1 trial, currently only need to
 %use it fors the first block for preintervention trials.
-function adaptData= AddingConditionsNirs(in,NewConditions,indexNewCond,description, oldConditionName)
+function in= AddingConditionsNirs(in, oldConditionName, newConditions, dataPath, newDecription, newTrialType)
+    if nargin < 5 || isempty(newDecription)
+        newDecription = 'Beginning of adaptation';
+    end
 
-% find the index for the label singleStanceSpeedFastAbsANK
-idxfast=find(compareListsNested({'singleStanceSpeedFastAbsANK'},in.data.labels)==1);
-idxslow=find(compareListsNested({'singleStanceSpeedSlowAbsANK'},in.data.labels)==1);
+    if nargin < 6 || isempty(newTrialType)
+        newTrialType = 'TM';
+    end
+    
+%     subplot(3,1,1)
+    in.plotAvgTimeCourse(in,'netContributionNorm2')
+    title('Before Changing Conditions');
+%     subplot(3,1,2)
+%     in.plotAvgTimeCourse(in,'singleStanceSpeedFastAbsANK')
+%     subplot(3,1,3)
+%     in.plotAvgTimeCourse(in,'singleStanceSpeedSlowAbsANK')
+    
+    % find the index for the label singleStanceSpeedFastAbsANK
+    idxfast=compareListsNested({'singleStanceSpeedFastAbsANK'},in.data.labels)==1;
+    idxslow=compareListsNested({'singleStanceSpeedSlowAbsANK'},in.data.labels)==1;
+    
+    trialNum = in.metaData.trialsInCondition{strcmp(in.metaData.conditionName,oldConditionName)};
+    columnIdxForTrialNum=find(compareListsNested({'Trial'},in.data.labels));
 
-trialNum = in.metaData.trialsInCondition{find(strcmp(in.metaData.conditionName,oldConditionName))};
-columnIdxTrialNum=find(compareListsNested({'Trial'},in.data.labels)==1);
-rowIdxForCurrentTrial = find(in.data.Data(:,columnIdxTrialNum) == trialNum);
+    fast=in.data.Data(:,idxfast);
+    slow=in.data.Data(:,idxslow);
+    difference=fast-slow;
 
-fast=in.data.Data(:,idxfast);
-slow=in.data.Data(:,idxslow);
-difference=fast-slow;
+    % find the index with speed difference or speed at 1 but also within the
+    % current condition of interes
+    idxSplit=difference>400 & in.data.Data(:,columnIdxForTrialNum) == trialNum;
 
-% find the index with speed difference or speed at 1 but also within the
-% current condition of interes
-idxSplit=find(difference>400);
-idxFast = f
-% in.metaData.trialsInCondition{3}=3;
-% in.metaData.trialsInCondition{4}=4;
+    currConditionLength = length(in.metaData.conditionName);
+    currMaxTrial = max(cell2mat(in.metaData.trialsInCondition));
 
-for i=1:length(NewConditions)
-    in.metaData.conditionName{i}=NewConditions{i};
-    in.metaData.trialsInCondition{i}=i;
-    in.metaData.conditionDescription{i}= description{i};
-    in.data.trialTypes{i}='TM';
-
-%     adaptData.metaData.conditionName{3}='Adaptation';
-%     adaptData.metaData.conditionName{4}='Washout';
-end
-in.data.Data(idxSplit(1):idxSplit(end),4)=indexNewCond(1);
-in.data.Data(idxSplit(end)+1:end,4)=indexNewCond(2);
-
-
-%%
-params={'stepLengthAsym'};
-in.plotAvgTimeCourse(in,params)
-
-adaptData=in;
-
-save([adaptData.subData.ID 'paramsNewConditionsV2.mat'],'adaptData','-v7.3');
+    %for now handles 1 new condition at a time
+    % ismember finds A in B 
+    newConditionTrialCount = 1;
+    [condExist, loc] = ismember(newConditions, in.metaData.conditionName);
+    condExistIdx = find(condExist);
+    loc = loc(condExistIdx);
+    if condExistIdx
+%         in.metaData.conditionName{currConditionLength+1}=in.metaData.conditionName{loc};
+        oldTrials = in.metaData.trialsInCondition{loc};
+        newCondTrials = nan(1,length(oldTrials)+newConditionTrialCount);
+        newCondTrials(1) = currMaxTrial + 1;
+        in.data.Data(idxSplit,columnIdxForTrialNum)=newCondTrials(1);
+        in.data.trialTypes{1}=in.data.trialTypes{1};
+        for i = 2:length(oldTrials)+1
+            newCondTrials(i) = currMaxTrial + i;
+            dataIdxToUpdate = in.data.Data(:,columnIdxForTrialNum) == oldTrials(i-1);
+            in.data.Data(dataIdxToUpdate,columnIdxForTrialNum) = newCondTrials(i);
+            in.data.trialTypes{end+1}=in.data.trialTypes{i-1};
+        end
+        
+        in.metaData.trialsInCondition{loc} = newCondTrials;
+%         in.metaData.conditionDescription{currConditionLength+1}= in.metaData.conditionDescription{loc};
+%         in.data.trialTypes{end+1}=in.data.trialTypes{loc};
+%         in.metaData.conditionName{loc} = 'Not used anymore';
+    else
+        in.metaData.conditionName{currConditionLength+1}=newConditions;
+        in.metaData.trialsInCondition{currConditionLength+1}=currMaxTrial+1;
+        in.metaData.conditionDescription{currConditionLength+1}= newDecription;
+        in.data.trialTypes{currConditionLength+1}=newTrialType;
+        in.data.Data(idxSplit,columnIdxForTrialNum)=currMaxTrial+1;
+    end
+    
+%     subplot(3,1,1)
+    in.plotAvgTimeCourse(in,'netContributionNorm2')
+    title('After Changing Params')
+%     subplot(3,1,2)
+%     in.plotAvgTimeCourse(in,'singleStanceSpeedFastAbsANK')
+%     subplot(3,1,3)
+%     in.plotAvgTimeCourse(in,'singleStanceSpeedSlowAbsANK')
+    
+%     adaptData = in;
+%     save([dataPath in.subData.ID 'paramsNewConditions.mat'],'adaptData','-v7.3');
 end 
